952105d4a704444c660d5344f206325684fac5bf
priyanshagra <127022024+priyanshagra@users.noreply.github.com>
2023-10-11 00:04:12 +0530

    fixed

--- client/src/components/AutoModal.js
+++ client/src/components/AutoModal.js
@@ -2,7 +2,8 @@ import {useState,useEffect} from "react"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 import { useCookies} from "react-cookie";
-import validator from 'validator';
+import validator from 'validator'
+
 const glassmorphismStyle = {
     background: 'rgba(255, 255, 255, 0.2)', 
     borderRadius: '10px',
@@ -10,6 +11,7 @@ const glassmorphismStyle = {
     backdropFilter: 'blur(10px)', 
     boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', 
   };
+
 const AuthModal = ({ setShowModal, isSignUp }) => {
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
@@ -60,7 +62,6 @@ const [passwordMessage,setMessage]=useState('Enter a password having minimum 8 c
 
     const handleSubmit = async (e) => {
 
-
         e.preventDefault()//page wont refresh .
     
         try {     
@@ -93,8 +94,8 @@ if(isSignUp && strongPassword==='false'){
     }
 
     return (
+
         <div className="auth-modal" style={glassmorphismStyle}>
-      
             <div className="close-icon" onClick={handleClick}>ⓧ</div>
             <h2>{isSignUp ? 'Create Account' : 'Log In'}</h2>
             <p>By clicking Log In, you agree to our terms. Learn how we process your data in our Privacy Policy and Cookie Policy.</p>
@@ -136,7 +137,7 @@ if(isSignUp && strongPassword==='false'){
             <hr/>
             <h2>GET THE APP</h2>
 
-    </div>
+        </div>
     )
 }
 export default AuthModal
