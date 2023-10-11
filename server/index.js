cfd9ec73da958df439569cdb20aadc860b16d195
priyanshagra <127022024+priyanshagra@users.noreply.github.com>
2023-10-11 22:55:03 +0530

    updated to add in database

--- server/index.js
+++ server/index.js
@@ -272,7 +272,7 @@ app.put('/user', async (req, res) => {
                 show_gender: formData.show_gender,
                 gender_identity: formData.gender_identity,
                 gender_interest: formData.gender_interest,
-                url: formData.url,
+                image_data: formData.url,
                 about: formData.about,
                 matches: formData.matches
             },
